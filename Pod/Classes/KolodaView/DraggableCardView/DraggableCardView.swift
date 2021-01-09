d94aef1fd0dc127e88b1f7f658111b7d9b1b4139
popai306 <fttttttttf@gmail.com>
2021-01-10 06:50:31 +0900

    fix

--- Pod/Classes/KolodaView/DraggableCardView/DraggableCardView.swift
+++ Pod/Classes/KolodaView/DraggableCardView/DraggableCardView.swift
@@ -484,7 +484,6 @@ public class DraggableCardView: UIView, UIGestureRecognizerDelegate {
             swipePositionAnimation?.removedOnCompletion = false
             swipePositionAnimation?.completionBlock = {
                 (_, _) in
-                self.removeFromSuperview()
                 completionHandler()
             }
             
